3c480244aff184fbcd80494c3e5d75b34f7972b7
kakarot98 <hrushikeshmohanty93@gmail.com>
2020-07-14 15:49:28 +0530

    plays basic game

--- main.js
+++ main.js
@@ -26,7 +26,11 @@ function startGame() {
 }
 
 function turnClick(square) {
-  turn(square.target.id, huPlayer);
+  if(typeof origBoard[square.target.id] == 'number'){
+    turn(square.target.id, huPlayer);
+    if (!checkTie()) turn(bestSpot(), aiPlayer);
+  }
+  
 }
 
 function turn(squareId, player) {
@@ -56,4 +60,31 @@ function gameOver(gameWon) {
   for (var i = 0; i < cells.length; i++) {
     cells[i].removeEventListener("click", turnClick, false);
   }
+  declareWinner(gameWon.player == huPlayer? "you win":"you lose");
+}
+
+function declareWinner(who) {
+  document.querySelector(".endgame").style.display = "block";
+  document.querySelector(".endgame .text").innerText = who;
+}
+
+function emptySquares() {
+  return origBoard.filter(s => typeof s == 'number');
+}
+
+
+function bestSpot(){
+  return emptySquares()[0];
+}
+
+function checkTie() {
+  if(emptySquares().length == 0) {
+    for(var i =0; i<cells.length; i++){
+      cells[i].style.backgroundColor = "lime";
+      cells[i].removeEventListener('click', turnClick, false);
+    }
+    declareWinner("Tie Game!");
+    return true;
+  }
+  return false;
 }
